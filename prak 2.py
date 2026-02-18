5ecc95883d3654c5f547203172296ff4ba2afdb8
BoraAbramov <borya812@gmail.com>
2026-02-18 19:37:29 +0200

    update

--- prak 2.py
+++ prak 2.py
@@ -47,16 +47,17 @@ print("\n📱 Languages for Mobile Development:")
 print(sorted(languages_mobile))
 
 
-print(f"\n❓ Which languages are good for BOTH Web AND Data Science?")
+print(f"\n❓ Which languages are good for BOTH Web AND Data Science?{languages_web & languages_data_science}")
 
-print(f"\n❓ Languages that can do Web, Mobile, OR Data Science?")
+print(f"\n❓ Languages that can do Web, Mobile, OR Data Science?{languages_mobile | languages_data_science | languages_mobile}")
 
-print(f"\n❓ Languages ONLY for System Programming (not web/mobile/data)?")
+print(f"\n❓ Languages ONLY for System Programming (not web/mobile/data)?{languages_systems - languages_data_science - languages_mobile - languages_web}")
 
-print(f"\n❓ Which languages work for Web but NOT Mobile?")
+print(f"\n❓ Which languages work for Web but NOT Mobile?{languages_web - languages_mobile}")
 
-print(f"\n❓ Is 'Python' in the web development set?")
+print(f"\n❓ Is 'Python' in the web development set?{"Python" in languages_web}")
 
 print(f"\n❓ The ultimate polyglot language (in ALL four categories)?")
 
-print(f"\n❓ Is modern_web a PROPER subset of web languages (PROPER = smaller, not equal)?")
+modern_web = {"JavaScript", "TypeScript", "Python"}
+print(f"\n❓ Is modern_web a PROPER subset of web languages (PROPER = smaller, not equal)?{modern_web < languages_web}")
